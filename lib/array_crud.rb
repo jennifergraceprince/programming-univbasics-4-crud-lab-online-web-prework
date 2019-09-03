2d8cec2a3bdf76e26824292ca5128bbe704e104d
Jennifer Prince <jennifergraceprince@gmail.com>
2019-09-03 04:32:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  retrieve_last_element_from_array = ["wow", "both", "arrays!"]
+  retrieve_last_element_from_array[2]
 end
 
 def update_element_from_index(array, index_number, element)
