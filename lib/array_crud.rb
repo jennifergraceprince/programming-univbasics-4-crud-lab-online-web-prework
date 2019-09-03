4fbbfbf1d218fe955ec6c69c1d5937ff74f85de2
Jennifer Prince <jennifergraceprince@gmail.com>
2019-09-03 04:29:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_element_from_index = ["hi", "am"]
+  retrieve_element_from_index = ["wow", "am"]
   retrieve_element_from_index[1]
 end
 
