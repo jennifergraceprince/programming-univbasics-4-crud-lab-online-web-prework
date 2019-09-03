13badc37ae804a3aa8b2817f7c66ea8bc2dd13be
Jennifer Prince <jennifergraceprince@gmail.com>
2019-09-03 04:28:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   retrieve_element_from_index = ["hi", "am"]
-  retrieve_element_from_index[2]
+  retrieve_element_from_index[1]
 end
 
 def retrieve_first_element_from_array(array)
