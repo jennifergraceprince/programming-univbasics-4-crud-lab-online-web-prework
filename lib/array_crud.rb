f507d9bf1a807293f689146b9f7409f4f743650e
Jennifer Prince <jennifergraceprince@gmail.com>
2019-09-03 04:14:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array.unshift(hungry)
-  p add_element_to_end_of_array
+  create_an_array.unshift("hungry")
 end
 
 def add_element_to_start_of_array(array, element)
