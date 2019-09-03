c89983e1d9714f579f553e00c04e42f72555f6df
Jennifer Prince <jennifergraceprince@gmail.com>
2019-09-03 04:32:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,7 @@ end
 
 def retrieve_first_element_from_array(array)
   retrieve_first_element_from_array = ["wow", "both"]
-  retrieve_first_element_from_index[0]
+  retrieve_first_element_from_array[0]
 end
 
 def retrieve_last_element_from_array(array)
