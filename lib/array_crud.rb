7b1234df55eeb02137c7b74a28a7245005182b41
Jennifer Prince <jennifergraceprince@gmail.com>
2019-09-03 04:25:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   remove_element_from_end_of_array = ["sup", "bye"]
-  remove_e
+  remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
