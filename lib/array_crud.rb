c319822eaaa96fdfdc5ae5442a9a2e13a0fa7546
Jennifer Prince <jennifergraceprince@gmail.com>
2019-09-03 04:15:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["hungry", "cat"]
-  add_element_to_end_of_array.unshift("hungry kitten")
+  add_element_to_end_of_array.unshift(bogus)
 end
 
 def add_element_to_start_of_array(array, element)
